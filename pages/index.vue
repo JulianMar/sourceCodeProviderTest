e4c2a3348838b33c6a7c2d2db8e0673c0dd4c0b5
Julian Martin <julian.martin@russmedia.com>
2020-07-15 10:39:52 +0200

    update to new version

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         sourceCodeProviderTest
       </h1>
       <h2 class="subtitle">
-        My impeccable Nuxt.js project
+        My impeccable Nuxt.js projects
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
